9e726fc30c1373dbc7481d96b1c59ea35d10e942
Ugwu Friday B <fugwungene@gmail.com>
2023-11-21 14:21:06 +0000

    Fix bugs in code.
    Append @localhost to username where necessary
    Append .* to performance_schema

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,7 +9,7 @@ USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges to new user on this database only
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev;
+GRANT ALL PRIVILEGES ON hbnb_dev_db TO hbnb_dev@localhost;
 
 -- Grant only select privilege to new user on performance schema
-GRANT SELECT ON performance_schema TO hbnb_dev;
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
