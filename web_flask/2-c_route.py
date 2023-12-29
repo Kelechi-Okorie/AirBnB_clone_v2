87adcf93900337e94464e68053f79d61ed6d7a35
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-12-29 10:52:37 +0100

    Added task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Returns a string for the index page"""
+
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Router for page /hbnb"""
+
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cisfun(text):
+    """Replaces a string variable"""
+
+    var = text.replace("_", " ")
+    return "C %s" % var
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
