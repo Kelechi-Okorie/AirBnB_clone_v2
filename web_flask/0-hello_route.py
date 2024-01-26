da75a1e128c24781fdd114d4bb3267deec47469e
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-01-26 19:15:30 +0000

    Added task 0 for 0x1A-application_server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbinb-onepage", strict_slashes=False)
 def index():
     """Returns a string for the index page"""
 
@@ -14,4 +14,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0:5000")
